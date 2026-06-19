00b21d6543ad9ea1b928660d442223e9f9c74f66
AD <burningggflamesss@gmail.com>
2026-06-19 13:00:59 +0545

    feat: implement login form with email and password authentication

--- src/routes/_onboard/login.tsx
+++ src/routes/_onboard/login.tsx
@@ -1,14 +1,40 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { authClient } from "#/packages/auth/auth-client.ts";
 
 export const Route = createFileRoute("/_onboard/login")({
 	beforeLoad: async ({ context }) => {
 		if (context.session) {
-			throw redirect({ to: "/" })
+			throw redirect({ to: "/" });
 		}
 	},
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/_onboard/login"!</div>;
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+		const email = formData.get("email") as string;
+		const password = formData.get("password") as string;
+
+		try {
+			await authClient.signIn.email({
+				email,
+				password,
+				callbackURL: "/",
+			});
+		} catch (error) {
+			console.log("Error: ", error);
+		}
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type="email" name="email" placeholder="Email" />
+
+			<input type="password" name="password" placeholder="Password" />
+
+			<button type="submit">Login Account</button>
+		</form>
+	);
 }
